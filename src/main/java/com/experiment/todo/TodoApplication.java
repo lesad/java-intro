226db2e1601f71eaa83d287a07ad7e8f49fc4722
Adrián Leštinský <adrian.lestinsky@morosystems.cz>
2023-03-07 12:42:29 +0100

    first tests

--- src/main/java/com/experiment/todo/TodoApplication.java
+++ src/main/java/com/experiment/todo/TodoApplication.java
@@ -7,13 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-
 @SpringBootApplication
 public class TodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TodoApplication.class, args);
+  }
 
   @Bean
   public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
