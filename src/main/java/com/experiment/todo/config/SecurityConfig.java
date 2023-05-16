1a2ba04e7f0bec58e9d42904d52122b6300f4442
Adrián Leštinský <adrian.lestinsky@morosystems.cz>
2023-05-16 11:11:23 +0200

    require authorization on all /users/** routes

--- src/main/java/com/experiment/todo/config/SecurityConfig.java
+++ src/main/java/com/experiment/todo/config/SecurityConfig.java
@@ -5,15 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests((authz) -> {
+      authz.requestMatchers("/**").authenticated();
+    }).httpBasic(withDefaults());
+
     return http.build();
   }
-
 }
-
