4544ff494d05dd371b22e5d76a3b6e178bce6e2a
Adrián Leštinský <adrian.lestinsky@morosystems.cz>
2023-10-18 16:00:04 +0200

    refactor security config

--- src/main/java/com/experiment/todo/config/SecurityConfig.java
+++ src/main/java/com/experiment/todo/config/SecurityConfig.java
@@ -2,25 +2,22 @@ package com.experiment.todo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests((authz) -> {
-      authz.requestMatchers("/admin/**").authenticated();
-      authz.requestMatchers("/users/**").permitAll();
-    }).httpBasic(withDefaults())
-      .cors(cors -> cors.disable())
-      .csrf(csrf -> csrf.disable());
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> {
+            authz.requestMatchers("/admin/**").authenticated();
+            authz.requestMatchers("/users/**").permitAll();
+        }).httpBasic(Customizer.withDefaults()).cors(cors -> cors.disable()).csrf(csrf -> csrf.disable());
 
-    return http.build();
-  }
+        return http.build();
+    }
 }
